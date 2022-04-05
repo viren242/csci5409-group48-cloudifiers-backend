e505124229795906c52ee417100d11fbd06ae836
Jay Patel <jy439129@dal.ca>
2022-04-04 22:55:56 -0300

    Added crossorigin annotation

--- src/main/java/com/cloudifiers/controller/UserManagementController.java
+++ src/main/java/com/cloudifiers/controller/UserManagementController.java
@@ -3,6 +3,7 @@ package com.cloudifiers.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,6 +28,7 @@ import io.swagger.annotations.ApiOperation;
 @Api(value = ControllerInfo.USER_MANAGEMENT_CONTROLLER_VALUE, description = ControllerInfo.USER_MANAGEMENT_CONTROLLER_DESC)
 @RestController
 @RequestMapping(CloudifiersConstants.BASE_API_URL)
+@CrossOrigin
 public class UserManagementController {
 
 	@Autowired
