e75d9d2c6033356f03364c8527706ac52507dcf2
Jay Patel <jy439129@dal.ca>
2022-04-04 21:57:58 -0300

    Added timeline generation

--- src/main/java/com/cloudifiers/service/ITimelineManagementService.java
+++ src/main/java/com/cloudifiers/service/ITimelineManagementService.java
@@ -1,5 +1,11 @@
 package com.cloudifiers.service;
 
+import java.util.List;
+
+import com.cloudifiers.entity.PostEntity;
+
 public interface ITimelineManagementService {
+
+	public List<PostEntity> generateTimeline(Integer userId);
 
 }
--- src/main/java/com/cloudifiers/service/TimelineManagementService.java
+++ src/main/java/com/cloudifiers/service/TimelineManagementService.java
@@ -1,8 +1,22 @@
 package com.cloudifiers.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.cloudifiers.entity.PostEntity;
+import com.cloudifiers.repository.PostRepository;
 
 @Service
 public class TimelineManagementService implements ITimelineManagementService {
+
+	@Autowired
+	private PostRepository postRepository;
+
+	@Override
+	public List<PostEntity> generateTimeline(Integer userId) {
+		return postRepository.getTimeline(userId);
+	}
 
 }
