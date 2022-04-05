c75b842306635ffd16c5eb4f386d99b865cc00f4
Jay Patel <jy439129@dal.ca>
2022-04-04 22:17:28 -0300

    Added file upload response

--- src/main/java/com/cloudifiers/controller/StorageController.java
+++ src/main/java/com/cloudifiers/controller/StorageController.java
@@ -1,9 +1,10 @@
 package com.cloudifiers.controller;
 
 import java.io.File;
-import java.net.URL;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,9 @@ import org.springframework.web.multipart.MultipartFile;
 import com.cloudifiers.constants.CloudifiersConstants;
 import com.cloudifiers.constants.CloudifiersConstants.ApiEndPoints;
 import com.cloudifiers.constants.CloudifiersConstants.ControllerInfo;
+import com.cloudifiers.model.Error;
+import com.cloudifiers.model.FileUploadResponse;
+import com.cloudifiers.model.ResponseStatus;
 import com.cloudifiers.service.IStorageService;
 import com.cloudifiers.util.Converter;
 
@@ -29,16 +33,20 @@ public class StorageController {
 	@Autowired
 	private IStorageService storageService;
 
-	@ApiOperation(value = ApiEndPoints.UPLOAD_FILE_DESC, response = String.class)
+	@ApiOperation(value = ApiEndPoints.UPLOAD_FILE_DESC, response = FileUploadResponse.class)
 	@PostMapping(ApiEndPoints.UPLOAD_FILE_URL)
-	public URL uploadFile(@RequestPart(value = "file") MultipartFile multipartFile) {
+	public ResponseEntity<ResponseStatus> uploadFile(@RequestPart(value = "file") MultipartFile multipartFile) {
 		File file = Converter.converMultipartFileToFile(multipartFile);
 		String fileName = Converter.generateUniqueName(file.getName());
 		storageService.uploadFile(fileName, file);
 		file.delete();
-		return storageService.getFileUrl(fileName);
+
+		String url = storageService.getFileUrl(fileName).toString();
+		FileUploadResponse fileUploadResponse = new FileUploadResponse(Boolean.TRUE, Error.NO_ERROR.getErrorCode(),
+				fileName, url);
+		return new ResponseEntity<ResponseStatus>(fileUploadResponse, HttpStatus.OK);
 	}
-	
+
 	@ApiOperation(value = ApiEndPoints.REMOVE_FILE_DESC, response = String.class)
 	@DeleteMapping(ApiEndPoints.REMOVE_FILE_URL)
 	public void removeFile(@PathVariable("fileName") String fileName) {
