96123fc88259342f588ff3eefba96560dfb8a69c
Jay Patel <jy439129@dal.ca>
2022-04-04 22:55:56 -0300

    Added crossorigin annotation

--- src/main/java/com/cloudifiers/controller/PostManagementController.java
+++ src/main/java/com/cloudifiers/controller/PostManagementController.java
@@ -3,6 +3,7 @@ package com.cloudifiers.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,6 +29,7 @@ import io.swagger.annotations.ApiOperation;
 
 @RestController
 @RequestMapping(CloudifiersConstants.BASE_API_URL)
+@CrossOrigin
 public class PostManagementController {
 
 	@Autowired
--- src/main/java/com/cloudifiers/controller/TimelineManagementController.java
+++ src/main/java/com/cloudifiers/controller/TimelineManagementController.java
@@ -3,6 +3,7 @@ package com.cloudifiers.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ import com.cloudifiers.service.ITimelineManagementService;
 import io.swagger.annotations.ApiOperation;
 
 @RestController
+@CrossOrigin
 public class TimelineManagementController {
 
 	@Autowired
